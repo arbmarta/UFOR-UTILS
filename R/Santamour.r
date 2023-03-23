398438d948a4cf5c84ef49640141babba24e0b0c
arbmarta <128729369+arbmarta@users.noreply.github.com>
2023-03-23 09:49:19 -0700

    Create Santamour.r

---
+++ R/Santamour.r
@@ -0,0 +1,5 @@
+# Write me a code to calculate the Santamour (10-20-30) rule for urban forestry biodiversty.
+Santamour_rule <- function (Santamour) {
+
+# Write me a code to calculate the Santamour (10-20-30) rule for urban forestry biodiversty in a pivot table
+Santamour_rule_pivot <- function (Santamour_pivot) {
